a64048e869d26121036b725ce6dfd7274ccac329
Thor133769 <Marcus.asberg.98@gmail.com>
2024-01-17 10:04:41 +0100

    First commit

--- data_analyser.py
+++ data_analyser.py
@@ -10,13 +10,25 @@ with open(file_path, "r") as file:
 # Convert JSON data to a Pandas DataFrame
 df = pd.DataFrame(sensor_data)
 
-# Display the DataFrame
+# Convert timestamp to datetime format
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+# Display the DataFrame with formatted timestamp
+df['formatted_timestamp'] = df['timestamp'].dt.strftime('%Y-%m-%d')
 print("DataFrame:")
-print(df)
+print(df[['formatted_timestamp', 'sensor_id', 'value']])
 
 # Data Analysis Example: Plotting
 for sensor_id, group in df.groupby("sensor_id"):
     plt.plot(group["timestamp"], group["value"], label=sensor_id)
+
+
+# Calculate the index of the middle row
+middle_index = len(df) // 2
+
+# Set x-axis ticks with the timestamp of the middle row
+plt.xticks([df['timestamp'].iloc[0], df['timestamp'].iloc[middle_index], df['timestamp'].iloc[-1]])
+
 
 plt.title("Sensor Data Analysis")
 plt.xlabel("Timestamp")
--- data_generator.py
+++ data_generator.py
@@ -2,13 +2,10 @@ import json
 import random
 from datetime import datetime, timedelta
 
-
 def generate_random_sensor_data(sensor_id):
     timestamp = datetime.utcnow().isoformat() + "Z"
     value = round(random.uniform(10.0, 30.0), 2)
     unit = "Celsius"
-    latitude = round(random.uniform(-90.0, 90.0), 6)
-    longitude = round(random.uniform(-180.0, 180.0), 6)
 
     data = {
         "timestamp": timestamp,
@@ -16,13 +13,10 @@ def generate_random_sensor_data(sensor_id):
         "value": value,
         "unit": unit,
         "location": {
-            "latitude": latitude,
-            "longitude": longitude
         }
     }
 
     return data
-
 
 def generate_random_sensor_data_batch(sensor_ids, num_samples):
     data_batch = []
@@ -34,7 +28,6 @@ def generate_random_sensor_data_batch(sensor_ids, num_samples):
 
     return data_batch
 
-
 if __name__ == "__main__":
     # Example: Generate random data for three sensors, each with 5 samples
     sensor_ids = ["temperature_sensor_1",
@@ -44,9 +37,13 @@ if __name__ == "__main__":
     random_data_batch = generate_random_sensor_data_batch(
         sensor_ids, num_samples)
 
-    # Save generated JSON data to a file
+    # Append generated JSON data to the existing file
     output_file = "random_sensor_data.json"
-    with open(output_file, "w") as file:
+    with open(output_file, "a") as file:
+        # Add a newline character before appending to separate batches
+        if file.tell() != 0:
+            file.write("\n")
         json.dump(random_data_batch, file, indent=2)
 
-    print(f"Generated data saved to {output_file}")
+    print(f"Generated data appended to {output_file}")
+
